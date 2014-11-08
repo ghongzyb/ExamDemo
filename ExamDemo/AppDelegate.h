79bddc6cce85287bdc80f2a3a9b891fa349726ea
ghongzyb <zhangchan5@sina.com>
2014-11-08 15:42:19 +0800

    first coding

---
+++ ExamDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExamDemo
+//
+//  Created by zyb on 14-11-8.
+//  Copyright (c) 2014年 zyb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExamDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExamDemo
+//
+//  Created by zyb on 14-11-8.
+//  Copyright (c) 2014年 zyb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
