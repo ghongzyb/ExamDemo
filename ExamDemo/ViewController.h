5766fcbcc927b6cd704a51a32c048b56d1c917dd
ghongzyb <zhangchan5@sina.com>
2014-11-08 17:49:21 +0800

    UIPicker  exam

--- ExamDemo/ViewController.h
+++ ExamDemo/ViewController.h
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIPickerViewDataSource, UIPickerViewDelegate>
 
+@property (strong, nonatomic) IBOutlet UIPickerView *myPicker;
+@property (nonatomic, strong) NSMutableArray *_data;
 
 @end
 
--- ExamDemo/ViewController.m
+++ ExamDemo/ViewController.m
@@ -8,20 +8,42 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
-
 @implementation ViewController
+
+
+//@synthesize myPicker;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"viewDidLoad");
     // Do any additional setup after loading the view, typically from a nib.
+    self.myPicker.delegate = self;
+    self.myPicker.dataSource = self;
+//    self.myPicker.frame = CGRectMake(0, 480, 320, 216);
+//    
+    NSArray *temp = @[@"pass",@"view",@"guess",@"kill",@"disable",@"loading",@"picker",@"contronller"];
+    self._data = [[NSMutableArray alloc] initWithArray:temp copyItems:YES];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+   
+    NSLog(@"%i",self._data.count);
+    return self._data.count;
+    
+}
+//
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 2;
+}
+//
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    return [self._data objectAtIndex:row];
 }
 
 @end
